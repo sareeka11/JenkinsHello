c1a07bd31c44a81143e0c9d029c47f15450a88b9
sareeka11 <65330164+sareeka11@users.noreply.github.com>
2020-07-28 14:39:18 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ package jenkins_training;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("Hello World of jenkins");
 	}
 
 }
